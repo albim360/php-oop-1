128155439ac9e9be2a563bb155bcf48eb74da777
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-30 15:44:21 +0200

    FEAT aggiunto construct

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <?php 
 
 class Movie { 
-    public $nome;
-    public $genere;
-    public $durata;
+    public string $nome;
+    public string $genere;
+    public string $durata;
+    public int $annouscita;
+
+    function __construct($nome) {
+        $this->nome = $nome;
+    }
 }
 
-$film1 = new Movie();
-$film1->nome = "Boolnception";
+$film1 = new Movie("Boolnception");
 $film1->genere = "Fantasy";
 $film1->durata = "2 ore";
+$film1->annouscita = "2023";
 
-$film2 = new Movie();
-$film2->nome = "Boolengers";
+
+$film2 = new Movie("Boolengers");
 $film2->genere = "Azione";
 $film2->durata = "5 ore";
+$film2->annouscita = "2022";
+
 
 var_dump($film1);
 var_dump($film2);
