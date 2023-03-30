e028dc110186345594611970fdf56249862be3cd
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-30 18:21:28 +0200

    FEAT aggiunta funzione con metodo + prezzo

--- index.php
+++ index.php
@@ -3,41 +3,39 @@
 class Movie { 
     public string $nome;
     public string $genere;
-    public string $durata;
-    public int $annouscita;
+    public int $durataOre;
+    public int $annoUscita;
+    public int $prezzo;
 
-    function __construct($nome, $genere) {
+    function __construct($nome, $genere, $durataOre, $annoUscita) {
         $this->nome = $nome;
         $this->genere = $genere;
+        $this->durataOre = $durataOre;
+        $this->annoUscita = $annoUscita;
+        $this->prezzo = $this->calcolaPrezzo();
+    }
+
+    function calcolaPrezzo() {
+        $prezzoPerOra = 2;
+        return $this->durataOre * $prezzoPerOra;
     }
 }
 
-$film1 = new Movie("Boolnception", "Fantasy");
-$film1->durata = "2 ore";
-$film1->annouscita = "2023";
+$film1 = new Movie("Boolnception", "Fantasy", 2, 2023);
+$film2 = new Movie("Boolengers", "Azione", 5, 2022);
 
+echo "<h2>Film 1:</h2>";
+echo "<p>Titolo: " . $film1->nome . "</p>";
+echo "<p>Genere: " . $film1->genere . "</p>";
+echo "<p>Durata: " . $film1->durataOre . " ore</p>";
+echo "<p>Anno di uscita: " . $film1->annoUscita . "</p>";
+echo "<p>Prezzo noleggio: €" . $film1->prezzo . "</p>";
 
-$film2 = new Movie("Boolengers", "Azione");
-$film2->durata = "5 ore";
-$film2->annouscita = "2022";
-
-
-var_dump($film1);
-var_dump($film2);
-
+echo "<h2>Film 2:</h2>";
+echo "<p>Titolo: " . $film2->nome . "</p>";
+echo "<p>Genere: " . $film2->genere . "</p>";
+echo "<p>Durata: " . $film2->durataOre . " ore</p>";
+echo "<p>Anno di uscita: " . $film2->annoUscita . "</p>";
+echo "<p>Prezzo biglietto: €" . $film2->prezzo . "</p>";
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP Movie</title>
-</head>
-<body>
-    <h1>  </h1>
-</body>
-</html>
